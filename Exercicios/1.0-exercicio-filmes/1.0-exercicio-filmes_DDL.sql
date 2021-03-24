11bf7b18814737963af82d81598912b2d8f70f02
Rafael-P <r4f4el_porto@hotmail.com>
2021-03-24 12:13:29 -0300

    adc

---
+++ Exercicios/1.0-exercicio-filmes/1.0-exercicio-filmes_DDL.sql
@@ -0,0 +1,24 @@
+-- Comandos DDL
+
+-- Cria um banco de dados chamado Filmes
+CREATE DATABASE Filmes;
+GO
+
+-- Define o banco de dados Filmes como o que será utilizado
+USE Filmes;
+GO
+
+CREATE TABLE Generos
+(
+	idGenero	INT PRIMARY KEY IDENTITY
+	,Nome		VARCHAR(200) NOT NULL -- NÃO NULO
+);
+GO
+
+CREATE TABLE Filmes
+(
+	idFilme		INT PRIMARY KEY IDENTITY
+	,idGenero	INT FOREIGN KEY REFERENCES Generos (idGenero)
+	,Titulo		VARCHAR(150) NOT NULL
+);
+GO
